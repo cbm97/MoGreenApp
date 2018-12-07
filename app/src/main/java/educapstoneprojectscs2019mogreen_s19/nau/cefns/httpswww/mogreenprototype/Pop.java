0a2838239f61a6990a28130df677b1569b0f22df
Chase M <36062839+cbm97@users.noreply.github.com>
2018-12-06 22:47:36 -0700

    Small fixes

--- app/src/main/java/educapstoneprojectscs2019mogreen_s19/nau/cefns/httpswww/mogreenprototype/Pop.java
+++ app/src/main/java/educapstoneprojectscs2019mogreen_s19/nau/cefns/httpswww/mogreenprototype/Pop.java
@@ -1,16 +1,29 @@
 package educapstoneprojectscs2019mogreen_s19.nau.cefns.httpswww.mogreenprototype;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 
 //Class for popup menu
 public class Pop extends Map_Menu {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
         protected void onCreate(Bundle savedInstanceState) {
+
+
 
             super.onCreate(savedInstanceState);
 
@@ -26,5 +39,10 @@ public class Pop extends Map_Menu {
             WindowManager.LayoutParams params = getWindow().getAttributes();
             params.gravity = Gravity.CENTER;
             getWindow().setAttributes(params);
+
+
         }
+
+
+
 }
