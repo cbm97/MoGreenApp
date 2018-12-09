2d9138f56773bc82efd2e6bfe5f5e2a7f87b4366
Chase M <36062839+cbm97@users.noreply.github.com>
2018-12-09 03:45:56 -0700

    Ability to log in, no sign up but have methods and resources to do so.

--- app/src/main/java/educapstoneprojectscs2019mogreen_s19/nau/cefns/httpswww/mogreenprototype/Login_Page.java
+++ app/src/main/java/educapstoneprojectscs2019mogreen_s19/nau/cefns/httpswww/mogreenprototype/Login_Page.java
@@ -1,25 +1,59 @@
 package educapstoneprojectscs2019mogreen_s19.nau.cefns.httpswww.mogreenprototype;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login_Page extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__page);
+        final EditText inputEmail = (EditText) findViewById(R.id.username);
+        final EditText inputPassword = (EditText) findViewById(R.id.password);
+        Button login = (Button) findViewById(R.id.login);
 
-        Button btn = (Button)findViewById(R.id.button);
+        //password authentication
+        mAuth = FirebaseAuth.getInstance();
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login_Page.this, Map_Menu.class));
+        login.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login_Page.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(Login_Page.this, Map_Menu.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+
+                        });
+
             }
+
         });
+
+
     }
+
+
+
 }
