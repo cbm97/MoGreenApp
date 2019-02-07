1658d0fa2f16f25142f6a433168349f5d86e2619
Jennie Ryckman <jtr93@nau.edu>
2019-02-06 18:24:52 -0700

    Added differentiation between reports and activity for tasks.

---
+++ app/src/main/java/educapstoneprojectscs2019mogreen_s19/nau/cefns/httpswww/mogreenprototype/Reports.java
@@ -0,0 +1,12 @@
+package educapstoneprojectscs2019mogreen_s19.nau.cefns.httpswww.mogreenprototype;
+
+public class Reports extends Pop {
+    String quickReport;
+    String fullReport;
+if(reportContent.equals("")){
+reportC = quickReport;
+    }
+    else{
+reportC = fullReport;
+    }
+}
